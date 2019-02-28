294a6124ba0967adf0a63d13dbe3357cc284d37a
UltimaPhoenix <fabio.grucci@gmail.com>
2019-02-28 15:02:09 +0100

    drop

---
+++ src/main/java/com/bedesign/hashcode2019/Pizza.java
@@ -0,0 +1,13 @@
+package com.bedesign.hashcode2019;
+
+public class Pizza {
+
+
+    public static int solve() {
+        return 0;
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/test/java/com/bedesign/hashcode2019/PizzaTest.java
@@ -0,0 +1,14 @@
+package com.bedesign.hashcode2019;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.*;
+
+
+class PizzaTest {
+
+    @Test
+    void solve() {
+        assertThat(Pizza.solve()).isEqualTo(0);
+    }
+}
