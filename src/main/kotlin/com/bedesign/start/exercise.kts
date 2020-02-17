4698480891394f7c02a922e9dcd009774a3f415c
Marco Manzi <mmanzi@soldo.com>
2020-02-17 19:20:28 +0100

    ExampleReady

--- src/main/kotlin/com/bedesign/start/exercise.kts
+++ src/main/kotlin/com/bedesign/start/exercise.kts
@@ -1,7 +1,9 @@
 import com.bedesign.framework.ExerciseScript
+import java.io.File
 
 val run = true
-
 ExerciseScript(Runnable {
-    println("This is the start for new exercises")
+    val lines = File("/Users/mmanzi/workspace/octopus/BeDesign-HC2019/src/main/resources/2020Trial/a_example.in").readLines()
+
+    println(lines[1].split(" ").map { it.toInt() }.sum())
 })
--- src/main/kotlin/com/bedesign/start/exerciseImpl.kt
+++ src/main/kotlin/com/bedesign/start/exerciseImpl.kt
@@ -1,9 +1,11 @@
 package com.bedesign.start
 import com.bedesign.framework.ExerciseScript
+import java.io.File
 
 val run = true
-
 val solution = ExerciseScript(Runnable {
-    println("This is the start for new exercises")
+    val lines = File("/Users/mmanzi/workspace/octopus/BeDesign-HC2019/src/main/resources/2020Trial/a_example.in").readLines()
+    
+    println(lines[1].split(" ").map { it.toInt() }.sum())
 })
 fun main() { solution.run() }
